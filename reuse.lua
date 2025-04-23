d1b4bc966e59504daacc6b57bce3b56fcf394faa
ywxoofc <109625340+ywxoofc@users.noreply.github.com>
2025-04-23 15:32:09 +0200

    Create reuse.lua

---
+++ reuse.lua
@@ -0,0 +1,29 @@
+if getgenv().AlreadyExecutedAutoLoader then
+    return
+end
+getgenv().AlreadyExecutedAutoLoader = true
+local function loadMainScript()
+    local function waitForGameToLoad()
+        if not game:IsLoaded() then
+            game.Loaded:Wait()
+        end
+    end
+    waitForGameToLoad()
+ 
+    local scriptUrl = "https://raw.githubusercontent.com/ywxoofc/gag/refs/heads/main/System3.lua"
+    loadstring(game:HttpGet(scriptUrl))()
+end
+local function setupAutoExecuteOnTeleport()
+    local queueOnTeleport = syn and syn.queue_on_teleport or queue_on_teleport or queueonteleport
+ 
+    if queueOnTeleport then
+        local autoLoaderUrl = "https://raw.githubusercontent.com/ywxoofc/gag/refs/heads/main/reuse.lua"
+        local autoLoaderScript = string.format('loadstring(game:HttpGet("%s"))()', autoLoaderUrl)
+ 
+        pcall(function()
+            queueOnTeleport(autoLoaderScript)
+        end)
+    end
+end
+task.spawn(setupAutoExecuteOnTeleport)
+loadMainScript()
